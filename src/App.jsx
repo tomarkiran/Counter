045ed93e020408a8bae20e2c3314528275505538
Kiran Tomar <tomarkiran559@gmail.com>
2025-08-28 12:09:10 +0530

    Corrected Mentioned Mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css'
+import React, {useState} from 'react'
 import Count from './components/Count'
 import ChangeBgColor from  './components/BgColor'
 
 function App() {
+    const[BgColor, setBgColor] = useState("white")
  
-
-  return (
+     return (
     
-      <div>
+      <div style={{backgroundColor:BgColor, height:"100vh" , width:"100vw", margin:0,padding:0}}>
         <Count/>
-        <ChangeBgColor/>
+        <ChangeBgColor onClick={()=> setBgColor('red')} label="Red "/>
+        <ChangeBgColor onClick={()=> setBgColor('blue')}label="blue"/>
+        <ChangeBgColor onClick={()=> setBgColor('green')}label="Green"/>
       </div>
-      
-  )
+    )
 }
-
 export default App
--- src/components/BgColor.jsx
+++ src/components/BgColor.jsx
@@ -1,18 +1,10 @@
-import React, {useState} from 'react'
+import React from 'react'
 
-const ChangeBgColor = ()=>{
-    const[BgColor, setBgColor] = useState("white")
-
-    return(
-        <div style={{  backgroundColor: BgColor, 
-        height: "100vh", 
-        width: "100vw",   
-        margin: 0,       
-        padding: 0  }}>
-            <button onClick={() => setBgColor('red')}>Red</button>
-            <button onClick={() => setBgColor('blue')}>Blue</button>
-            <button onClick={() => setBgColor('green')}>Green</button>
-        </div>
-    )
+const ChangeBgColor = ({onClick, label})=>{
+ return(
+       <div>
+           <button onClick={onClick}>{label}</button>
+       </div>
+   )
 }
 export default ChangeBgColor
